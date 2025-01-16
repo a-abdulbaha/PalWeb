2411ddf212367239d58cea20f4306d6c67e2c20a
Shift <shift@laravelshift.com>
2025-01-16 15:19:57 +0000

    Default new `bootstrap/app.php`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,38 +1,20 @@
 <?php
 
-use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        \SocialiteProviders\Manager\ServiceProvider::class,
-        \Spark\SparkServiceProvider::class,
-    ])
+    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn () => route('unauth'));
-        $middleware->redirectUsersTo(AppServiceProvider::HOME);
-
-        $middleware->web([
-            \App\Http\Middleware\SetSiteLocale::class,
-            \App\Http\Middleware\TrackPageViews::class,
-        ]);
-
-        $middleware->throttleApi();
-
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\MustBeAdministrator::class,
-            'pageDescription' => \App\Http\Middleware\PageDescription::class,
-            'pageTitle' => \App\Http\Middleware\PageTitle::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
